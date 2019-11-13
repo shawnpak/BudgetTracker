67b5e774f4d5a11b72de1a07401f01c09cc6ef9d
shawnkek <shawnkek@cs.students.ubc.ca>
2019-11-13 14:05:15 -0800

    -implemented money and bills expenses -implemented load and save functions -updated tests for new interfaces

--- src/main/ui/Printer.java
+++ src/main/ui/Printer.java
@@ -8,15 +8,21 @@ import java.text.DecimalFormat;
 
 public class Printer implements Observer {
     DecimalFormat df = new DecimalFormat("#.00");
+    private boolean updated;
 
     public Printer() {
+        updated = false;
 
+    }
+
+    public Boolean getUpdated() {
+        return updated;
     }
 
     @Override
     public void update(Expenses e, double stats) {
         System.out.println("Adding expense: " + e.getType() + " - " + e.getExpense());
         System.out.println("You have used up " + df.format(stats) + "% of your budget this month!");
-
+        updated = true;
     }
 }
---
+++ src/test/observer/SubjectTest.java
@@ -0,0 +1,44 @@
+package observer;
+
+import model.Expenses;
+import model.exception.LargeNumberException;
+import model.exception.NegativeInputException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ui.BudgetApp;
+import ui.Printer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubjectTest {
+    private Subject s;
+    private Printer p;
+    private Expenses e;
+    @BeforeEach
+    void runBefore() {
+        s = new BudgetApp();
+        p = new Printer();
+        s.addObserver(p);
+        try {
+            e = new Expenses("1", 1);
+        } catch (NegativeInputException ex) {
+            ex.printStackTrace();
+        } catch (LargeNumberException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Test
+    void testAddObserver() {
+        assertTrue(s.observers.contains(p));
+        s.addObserver(p);
+        assertEquals(s.observers.size(), 1);
+    }
+    @Test
+    void testNotifyObservers() {
+        assertFalse(p.getUpdated());
+        s.notifyObservers(e,1.0);
+        assertTrue(p.getUpdated());
+    }
+
+}
